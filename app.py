ec7dedc118cb4263de4544e6ed2dc25817cfb4f4
pozsa <krisztian.pozsa@gmail.com>
2018-05-20 14:53:36 +0200

    add app.py and 7.png
    7.png is a test image
    app.py is the flask api

---
+++ app.py
@@ -0,0 +1,39 @@
+import flask
+import keras
+import numpy as np
+import PIL
+import tensorflow as tf
+
+app = flask.Flask(__name__)
+model = keras.models.load_model('digit_model.h5')
+graph = tf.get_default_graph()
+
+@app.route('/')
+def index():
+    inst = '''
+    curl -F 'file=@/path/to/digit_pic.png' {}
+    '''.format(flask.url_for('predict', _external=True))
+    return inst
+
+
+@app.route('/status')
+def status():
+    return flask.jsonify({'status': 'Ok'})
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    fp = flask.request.files['file']
+    image = PIL.Image.open(fp)
+    image = np.asarray(image.resize((56, 56)))
+    image = image.reshape(1, 56, 56, 1)
+
+    with graph.as_default():
+        prediction = model.predict(image)
+
+    digit = np.argmax(prediction)
+    return flask.jsonify({'digit': int(digit)})
+
+
+if __name__ == '__main__':
+    app.run()
